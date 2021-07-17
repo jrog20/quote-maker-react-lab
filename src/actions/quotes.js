d7954bfbc9a0b76a834cf2a9bc9f1cbe43fd214d
jrog20 <jaimerogstad@gmail.com>
2021-07-17 19:56:49 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,31 @@
-// TODO: Create action creators as defined in tests
+// return an Object with a type of "ADD_QUOTE" and a quote object
+export const addQuote = quote => {
+  return {
+    type: 'ADD_QUOTE',
+    quote: Object.assign({}, quote, { votes: 0 })
+  }
+}
+
+// return an Object with a type of "REMOVE_QUOTE" and a quoteId
+export const removeQuote = quoteId => {
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteId
+  }
+}
+
+// return an Object with a type of "UPVOTE_QUOTE" and a quoteId
+export const upvoteQuote = quoteId => {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId
+  }
+}
+
+// return an Object with a type of "DOWNVOTE_QUOTE" and a quoteId
+export const downvoteQuote = quoteId => {
+  return {
+    type: 'DOWNVOTE_QUOTE',
+    quoteId
+  }
+}
